6054e0532592a28fa0832f4d73c0dfb112c7bb37
Valerii Oboyanskyi <lollipopmila@gmail.com>
2024-07-08 15:55:07 +0200

    minor corrections before submitting for review

--- todo/urls.py
+++ todo/urls.py
@@ -14,9 +14,21 @@ from .views import (
 urlpatterns = [
     path("", TaskListView.as_view(), name="task_list"),
     path("new/", TaskCreateView.as_view(), name="task_create"),
-    path("task/<int:pk>/edit/", TaskUpdateView.as_view(), name="task_update"),
-    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
-    path("task/<int:task_id>/toggle/", views.task_toggle, name="task_toggle"),
+    path(
+        "task/<int:pk>/edit/",
+        TaskUpdateView.as_view(),
+        name="task_update"
+    ),
+    path(
+        "task/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task_delete"
+    ),
+    path(
+        "task/<int:task_id>/toggle/",
+        views.task_toggle,
+        name="task_toggle"
+    ),
     path("tags/", TagListView.as_view(), name="tags_list"),
     path("tag/new/", TagCreateView.as_view(), name="tag_new"),
     path("tag/<int:pk>/edit/", TagUpdateView.as_view(), name="tag_edit"),
